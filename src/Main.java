35dd696b9764abb1bd540d7b3d7b7f5be3b2e94e
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-23 18:36:57 +0600

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Test test = new Test();
+        System.out.println("test.test = " + test.test);;
     }
 }
