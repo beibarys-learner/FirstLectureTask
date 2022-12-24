38bde6c8142d36659226e9195fc5e4fa6494bdd2
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-24 15:19:11 +0600

    empty car class, delete all main code

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car("Teana", "sedan", "black");
-        // here we created car with predefined fields
-//        Car bmw = new Car();
 
-        // we rewrite the previous project using setters
-        // we set fields of Car
-        //        nissan.setName("teana");
-        //        nissan.setModel("sedan");
-        //        nissan.setColor("black");
-        nissan.setNum1(5);
-        nissan.setNum2(210);
-        // since we added getter and set for num1 and num2
-
-        nissan.drive();
-
-        nissan.sumNumbers(1, 2);
-
-        // we print message by getting Name
-        System.out.println("nissan's name will be " + nissan.getName() + " printed using getName");
-
+        Car car = new Car();
+        car.setName("car");
+        car.drive();
     }
 }
