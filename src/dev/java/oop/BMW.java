60129195afc22f85a74cd44e2d98e1752a541803
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-24 18:21:52 +0600

    Homework description in readme.md

--- src/dev/java/oop/BMW.java
+++ src/dev/java/oop/BMW.java
@@ -1,8 +1,17 @@
 package dev.java.oop;
 
 public class BMW extends Car{
+    public BMW() {
+    }
+
+    public BMW(String name, String model, String color) {
+        super(name, model, color);
+    }
+
     @Override
     public void drive() {
+        super.signal(); // recalled from car
+        super.drive();
         System.out.println("bmw drive");;
     }
 }
